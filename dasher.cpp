98dd422bbed8be5cd58563333e319a1f88d8942f
Aaron <fontenot.aaron3@gmail.com>
2026-02-10 23:44:05 -0600

    Added gravity and BANNED double jumps

--- dasher.cpp
+++ dasher.cpp
@@ -6,14 +6,33 @@ int main() {
 
     InitWindow(window_width, window_height, "Dapper Dasher");
 
+    // rectangle values
+    const int width{50};
+    const int height{80};
+    int posY{window_height - height};
+    
+    int y_velocity{0};
+    int x_velocity{0};
+    const int gravity{1};
+
     SetTargetFPS(60);
     while (!WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(WHITE);
         //Game Logic Begins
+        posY += y_velocity;
+        if (posY >= (window_height - height)) { 
+            y_velocity = 0;
+        } else {
+            y_velocity += gravity; 
+        }    
+        
+        // Jump Logic
+        if (IsKeyPressed(KEY_SPACE) && posY == window_height - height) {
+            y_velocity -=10;
+        }
 
-
-
+        DrawRectangle(window_width / 2, posY, width, height, BLUE);
 
         EndDrawing();
     }
